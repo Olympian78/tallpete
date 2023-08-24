a85920f26da59012e5c917fbf09aaf1904443053
Ian de Villiers <78476342+Olympian78@users.noreply.github.com>
2023-08-24 12:46:32 +0200

    Update invoices-st.py
    Replace pk_unit with qty

--- invoices-st.py
+++ invoices-st.py
@@ -2,14 +2,13 @@ import fitz
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Cranberry Extract Invoices')
+st.set_page_config(page_title="Cranberry Extract Invoices")
 
-order_df = pd.DataFrame(columns=['order_no', 'document_date', 'store_name', 'pk_unit'], dtype=str)
+order_df = pd.DataFrame(columns=["order_no", "document_date", "store_name", "qty"], dtype=str)
 
 # fname = "PO - 4801417909.pdf"
-files = st.file_uploader(label='Select files to upload', type='pdf', accept_multiple_files=True)
+files = st.file_uploader(label="Select files to upload", type="pdf", accept_multiple_files=True)
 for i, ff in enumerate(files, start=1):
-    # st.write(ff.getbuffer())
     doc = fitz.open(stream=ff.getvalue())
     page = doc[0]
     text = page.get_text("blocks", sort=True)
@@ -18,19 +17,20 @@ for i, ff in enumerate(files, start=1):
     # #     # for j, row in enumerate(block[-4]):
     # #     #     print(i, j, row)
 
-    order_no = list(filter(lambda x: x != '', text[0][4].split('\n')))[0].split()[-1]
-    document_date = list(filter(lambda x: x != '', text[3][4].split('\n')))[-1].split()[-1]
-    store_name = list(filter(lambda x: x != '', text[5][4].split('\n')))[-1].split(':')[-1].replace('-', '')
-    pk_unit = list(filter(lambda x: x != '', text[11][4].split('\n')))[3].strip()
+    order_no = list(filter(lambda x: x != "", text[0][4].split("\n")))[0].split()[-1]
+    document_date = list(filter(lambda x: x != "", text[3][4].split("\n")))[-1].split()[-1]
+    store_name = list(filter(lambda x: x != "", text[5][4].split("\n")))[-1].split(":")[-1].replace("-", "")
+    # text
+    qty = list(filter(lambda x: x != "", text[11][4].split("\n")))[4].strip()
 
-    order_df.loc[i] = [order_no, document_date, store_name, pk_unit]
+    order_df.loc[i] = [order_no, document_date, store_name, qty]
 
     # st.write(f'{order_no = }')
     # st.write(f'{document_date = }')
     # st.write(f'{store_name = }')
     # st.write(f'{pk_unit = }')
 
-st.write('Order preview:')
+st.write("Order preview:")
 st.write(order_df)
 
-st.download_button('Download .txt (CSV) file', order_df.to_csv(index=False))
+st.download_button("Download .txt (CSV) file", order_df.to_csv(index=False))
